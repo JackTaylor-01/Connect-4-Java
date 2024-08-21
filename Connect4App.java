80548ffca3523e63106642bf9beaf3290ce49129
JackTaylor-01 <jackt4650@gmail.com>
2024-08-21 23:21:22 +0100

    allowed player configuration for grid size and token to win

--- Connect4App.java
+++ Connect4App.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class Connect4App {
     public static void main(String[] args){
 
-        Connect4 game = new Connect4(10, 10, 4);
+        int rows = 10;
+        int columns = 10;
+        int winningLength = 4;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter row amount: ");
+        rows = sc.nextInt();
+        System.out.println("Please enter column amount: ");
+        columns = sc.nextInt();
+        System.out.println("Please enter winning length: ");
+        winningLength = sc.nextInt();
+
+        Connect4 game = new Connect4(rows, columns, winningLength);
         game.playGame();
     }
     
